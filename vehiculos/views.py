754ed065d14813173e83e409a422ace8ad8ecb17
josedavid019 <joseangaritae@gmail.com>
2025-03-25 19:03:39 -0500

    Gestion de vehiculos y de clientes casi lista

--- vehiculos/views.py
+++ vehiculos/views.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets, filters
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
 from .models import Vehiculo
 from .serializers import VehiculoSerializer
 
@@ -7,8 +6,3 @@ from .serializers import VehiculoSerializer
 class VehiculoViewSet(viewsets.ModelViewSet):
     queryset = Vehiculo.objects.all()
     serializer_class = VehiculoSerializer
-
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['marca', 'tipo', 'estado', 'ubicacion']
-    search_fields = ['marca', 'modelo', 'placa']
-    ordering_fields = ['precio_alquiler_dia', 'anio', 'kilometraje']
